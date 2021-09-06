5d0238f277aaaaeaf0d6094d519c2d7966888c3b
brielino <61842589+brielino@users.noreply.github.com>
2021-09-06 12:16:40 +0200

    Update stazionaria.c

--- Codici/stazionaria.c
+++ Codici/stazionaria.c
@@ -21,7 +21,7 @@
 #define STOP 30000.0            /* terminal (close the door) time       */
 #define INFINITE (100.0 * STOP) /* must be much larger than STOP        */
 #define SERVERS 5
-#define LAMBDA 15                /* Traffic flow rate                    */
+#define LAMBDA 15               /* Traffic flow rate                    */
 #define ALPHA 1.5               /* Shape Parameter of BP Distribution   */
 #define N (LAMBDA *30000)
 #define K 64
@@ -65,8 +65,6 @@ int streams = 1;
 long departures = 0;
 int current_batch = 0; // a quale batch ci troviamo
 
-//Output Statistics Struct
-sum statistics;
 
 // Event List Management
 event_list event;
@@ -215,51 +213,55 @@ int NextEvent(event_list event)
 
 int main(void) {
 
-    for(int f=1; f<=10;f++){
+    for(int f=1; f<=10; f++){
+        int departures_batch = 0;               
+        PlantSeeds(46464);
+
+        clock.current    = START;
         streams = f*2;
-        //Per eliminare le statstiche precedenti
         current_batch = 0;
         arrival = START;
+        departures = 0;
+        number[0] = 0;
+        number[1] = 0;
+        number[2] = 0;
+        number[3] = 0;
+        number[4] = 0;
         for(int current_batch_index=0 ; current_batch_index<64 ; current_batch_index++){
             for(int z=0; z<SERVERS;z++){ 
                 s_batch[current_batch_index][z].area=0.0;
                 s_batch[current_batch_index][z].departures=0;
             } 
-        }
-        
-        
-        double current_time_batch = START; //per verificare che il tempo del batch                   
-        // Init
-        PlantSeeds(46464);
+        }   
+        //To delete the previus statistics
 
-        clock.current    = START;        // set the clock
-        
-        event[0].t   = GetArrival();     // schedule the first arrival
+        event[0].t   = GetArrival();     
         event[0].x = 1;
         for (int s = 1; s <= SERVERS; s++) { 
             event[s].t     = INFINITE;
             event[s].x     = 0;          // Departure process is off at the start
-            statistics[s].service = 0.0;
-            statistics[s].served = 0;
         }
 
         int e = 0;
-        while ((event[0].t < STOP) || !empty_queues()) { 
+        while (departures < N) { 
             e = NextEvent(event);
             clock.next = event[e].t;
             
-            if(current_batch<K){
-                for(int z=0; z<SERVERS;z++){ //prendo le analisi di tutti i batch
-                    s_batch[current_batch][z].area+=  (clock.next - clock.current) * number[z];
+            if(current_batch < K){
+                for(int z=0; z< SERVERS ;z++){ //Take all batch's analyses
+                    if(number[z]>0){
+                        s_batch[current_batch][z].area+=  (clock.next - clock.current) * number[z];
+                    }
                 } 
             }
-            if(B < current_time_batch && current_batch < K){
-                // per passare al prossimo batch
+            if(B < departures_batch && current_batch < K){
+                // Process next Batch
                 current_batch++;
+                departures_batch = 0;
                 
             }
             clock.current = clock.next;
-            current_time_batch = clock.current-B*current_batch;
+            departures_batch++;
             if (e == 0) {
                 // Process an Arrival
                 arrivals++;
@@ -287,7 +289,6 @@ int main(void) {
         }
 
         for (int z = 0; z < K ; z++){
-            //printf("Batch n° %d\n",z + 1);
             double avg_wait = (s_batch[z][0].area / s_batch[z][0].departures +
                         s_batch[z][1].area / s_batch[z][1].departures + 
                         s_batch[z][2].area / s_batch[z][2].departures + 
@@ -297,7 +298,7 @@ int main(void) {
             b_intervall[z].avg_wait = avg_wait;
             
         }
-        for(int i = 0; i< 64; i++){
+        for(int i = 0; i< K; i++){
             printf("%f\n",b_intervall[i].avg_wait);
         }
         printf("\n\n");
